08a4302b827c0f56447e1399dede20300c407ba0
SantiB <151892199+SanB00@users.noreply.github.com>
2026-08-26 01:05:52 -0300

    de Albano Suarez canosa - lunes, 24 de agosto de 2026, 23:23 Número de respuestas: 1 Buenas noches grupo continuando con el ejercico numero 2 posteo en el nuevo hilo los aportes realizados por lautaro , Recuerden que por cada ejercicio debemos crear un hilo nuevo
    image.png
    Tomando como refrencia estos aportes , Agregue el metodo toString() y los constructores vacios de persona y empleados.
    
     image%20%281%29.png
    image%20%282%29.png
    
     TP1_GRUPO_16 v7.rar

--- src/ejercicio1/Persona.java
+++ src/ejercicio1/Persona.java
@@ -3,8 +3,6 @@ package ejercicio1;
 import java.time.LocalDate;
 
 public class Persona {
-	
-	//Atributos
 	private String dni;
 	private String nombre;
 	private String apellido;
@@ -15,26 +13,14 @@ public class Persona {
 	private String telefono;
 	private String email;
 	
-	//Constructores
-	
-	public Persona()
-	{
-		dni = "";
-		nombre = "sin nombre";
-		apellido = "";
-		fechaNacimiento = LocalDate.of(2025, 1, 1);
-		genero = "";
-		direccion = "";
-		telefono = "";
-		email = "";
-		edad = 99;
+	public Persona() {
+		
 	}
-	
-	public Persona(String dni, String nombre, String apellido, int edad, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email) {
+	public Persona(String dni,String nombre,String apellido,LocalDate fechaNacimiento
+			,String genero,String direccion,String telefono,String email) {
 		this.dni=dni;
 		this.nombre=nombre;
 		this.apellido=apellido;
-		this.edad=edad;
 		this.fechaNacimiento=fechaNacimiento;
 		this.genero=genero;
 		this.direccion=direccion;
@@ -42,42 +28,5 @@ public class Persona {
 		this.email=email;
 	}
 	
-	//Encapsulamiento
-	
-	public String getDni() { return dni; }
-	public void setDni(String dni) { this.dni = dni; }
-
-	public String getNombre() { return nombre; }
-	public void setNombre(String nombre) { this.nombre = nombre; }
-
-	public String getApellido() { return apellido; }
-	public void setApellido(String apellido) { this.apellido = apellido; }
-
-	public int getEdad() { return edad; }
-	public void setEdad(int edad) { this.edad = edad; }
-
-	public LocalDate getFechaNacimiento() { return fechaNacimiento; }
-	public void setFechaNacimiento(LocalDate fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
-
-	public String getGenero() { return genero; }
-	public void setGenero(String genero) { this.genero = genero; }
-
-	public String getDireccion() { return direccion; }
-	public void setDireccion(String direccion) { this.direccion = direccion; }
-
-	public String getTelefono() { return telefono; }
-	public void setTelefono(String telefono) { this.telefono = telefono; }
-
-	public String getEmail() { return email; }
-	public void setEmail(String email) { this.email = email; }
-	    
-	//Metodo ToString
-	    
-        public String toString() {
-        	 return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido +
-                     ", edad=" + edad + ", fechaNacimiento=" + fechaNacimiento +
-                     ", genero=" + genero + ", direccion=" + direccion +
-                     ", telefono=" + telefono + ", email=" + email + "]";
-        }
 
 }
