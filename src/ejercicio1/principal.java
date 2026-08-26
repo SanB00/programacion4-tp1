2df0c28ba57691478da32218f6d58e084678b9dc
SantiB <151892199+SanB00@users.noreply.github.com>
2026-08-26 01:25:11 -0300

    Syntax error, insert "}" to complete ClassBody

--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -1,10 +1,24 @@
 package ejercicio1;
 
+import java.time.LocalDate;
+
 public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Empleado obj = new Empleado(
+	            "12345678A",                          // dni
+	            "Juan",                               // nombre
+	            "Pérez",                              // apellido
+	            LocalDate.of(1990, 5, 15),           // fechaNacimiento (Año, Mes, Día)
+	            "Calle Falsa 123",                    // direccion
+	            "Masculino",                          // genero
+	            "+34600112233",                       // telefono
+	            "juan.perez@email.com",               // email
+	            "Desarrollador Java",                 // puesto
+	            10452                                 // legajo
+	        );
+		System.out.println(obj);
 	}
 
 }
